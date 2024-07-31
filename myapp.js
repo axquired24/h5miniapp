10553afe5f916e5bcbed8156871da2b19a57c4bb
Albert S <albertseptiawan@ayoconnect.id>
2024-07-31 10:26:13 +0700

    feat: initial check

---
+++ myapp.js
@@ -0,0 +1,68 @@
+function alertCheck() {
+  my.alert({
+    title: 'Test Alert!!',
+    content: window.navigator.userAgent,
+    buttonText: 'Alert Button',
+    success: function (res) {
+      my.alert({
+        content: 'success!' + JSON.stringify(res),
+      });
+    },
+    fail: function () {
+      my.alert({
+        content: 'fail!',
+      });
+    },
+    complete: function () {
+      my.alert({
+        content: 'complete!',
+      });
+    },
+  });
+}
+
+function authCheck() {
+  my.getAuthCode({
+    scopes: ['auth_base'],
+    success: (res) => {
+      my.alert({
+        content: res.authCode,
+      });
+    },
+    fail: (res) => {
+        my.alert({
+          content: "Failed to run my.getAuthCode, " + JSON.stringify(res),
+        });
+        console.log(res.authErrorScopes)
+    },
+  });
+}
+
+function tradePay() {
+  my.tradePay({
+    tradeNO: '201711152100110410533667792', // get the tradeNo from the server first
+    success: (res) => {
+      my.alert({
+        content: JSON.stringify(res),
+      });
+    },
+    fail: (res) => {
+      my.alert({
+        content: JSON.stringify(res),
+      });
+    }
+  });
+}
+
+(function() {
+  const listenerList = [
+    ['AlertBtn', alertCheck],
+    ['AuthBtn', authCheck],
+    ['PayBtn', tradePay]
+  ]
+
+  listenerList.forEach(el => {
+    const tempVar = document.getElementById(el[0])
+    tempVar.addEventListener('click', el[1])
+  });
+})();
